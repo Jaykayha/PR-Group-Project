d203ae5472ac4b2a5c5b69a88f5d92e48de477d0
nickipan <57984917+nickipan@users.noreply.github.com>
2023-01-21 22:24:55 +0100

    Function working?

--- Function.py
+++ Function.py
@@ -1,17 +1,48 @@
 import pandas as pd
+import numpy as np
 import os
+import sys
 
-path = "C:/Users/Nicki/Documents/GitHub/PR-Group-Project"
-os.chdir(path)
+os.chdir(os.path.dirname(sys.argv[0]))
 
 df = pd.read_excel('Final responses spreadsheet.xlsx')
-df = df.drop(df.columns[0], axis=1)
 
-every_fourth_column = df.iloc[:, ::4]
-columns = every_fourth_column.columns
+filtered_df1 = df.filter(regex='^Question ')
+filtered_df2 = df.filter(regex='^[0-9]')
 
-for x in columns:
-    # print(df[=".  Is this an action movie?"])
-    print(df[x])
+n_rows = df.shape[0]
+timestamps = []
+predictions = []
+maps = []
+ids =[940, 3073, 6545, 6912, 11360, 12015, 15783, 23413, 24283, 30364, 30625, 32357, 33433, 34789, 41850]
+movie_ids = []
 
-# df = df.assign(list_0_survey=[], list_1_survey=[])
+for x in range(0, n_rows):
+    counter = 0
+    id_counter = 0
+    for y in filtered_df2.columns:
+        a = np.zeros((3,3))
+        timestamps.append(df["Timestamp"][x])
+        pred = 1 if df[y][x] == "Yes, it is action" else 0
+        predictions.append(pred)
+        movie_ids.append(ids[id_counter])
+        id_counter += 1
+
+        for z in range(0, 3):
+            if(type(df[filtered_df1.columns[counter]][x]) == float):
+                elementList = []
+            else:
+                elementList = df[filtered_df1.columns[counter]][x].split()
+                for l in elementList:
+                    if l == "A," or l == 'A':
+                        a[z,0] = 1
+                    if l == "B," or l == 'B':
+                        a[z,1] = 1
+                    if l == "C":
+                        a[z,2] = 1
+            counter+= 1
+        maps.append(a)
+            
+data = {"Timestamp": timestamps, "Movie ID": movie_ids, "Prediction": predictions, "Map": maps}
+df2 = pd.DataFrame(data)
+print(df2)
